d42fcb999ef9e34b2bdb6663154ff4042350d89b
AllTeach <33581448+AllTeach@users.noreply.github.com>
2018-05-13 22:34:05 +0300

    Add files via upload

---
+++ pureJSServer.js
@@ -0,0 +1,122 @@
+const http = require('http');
+ var fs = require('fs');
+var querystring = require('querystring');
+		// testing working woth JSON data as DB START
+	
+	var dj = [
+{
+user:
+	{
+	'username':'xz',
+	'password':'xz@123',
+	'email':'xyz@xz.com',
+	'uid': 1100
+	}
+},
+{
+user:
+	{
+	'username':'yz',
+	'password':'yz@123',
+	'email':"xyz@yz.com",
+	'uid': 1200
+	}
+}
+]
+
+function processPost(request,response,callback)
+
+{
+	let body = [];
+	  if(request.method == 'POST') {
+		  request.on('error', (err) => {
+			console.error(err);
+		  }).on('data', function(chunk) {
+			  console.log("chunk: " + chunk);
+			body.push(chunk);
+		  }).on('end', function() {
+			request.post = Buffer.concat(body).toString();
+			callback();
+			
+			  });
+	  }
+	
+}
+ 
+http.createServer((request, response) => {
+	
+	//console.log(request);
+  const { headers, method, url} = request;
+  
+    // At this point, we have the headers, method, url and body, and can now
+    // do whatever we need to in order to respond to this request.
+	console.log(url);
+	
+	response.on('error', (err) => {
+			  console.error(err);
+			});
+			
+	
+	switch(url)
+	{
+		case '/clientjs.html':
+			var filename = "." + url;
+	
+			fs.readFile(filename, function(err, data) {
+			if (err) {
+				console.log('error ' + err);
+				response.writeHead(404, {'Content-Type': 'text/html'});
+				response.end();
+			} 
+			else
+			{
+				
+				console.log('sending home.html');
+				response.writeHead(200, {'Content-Type': 'text/html'});
+				response.write(data);
+				response.end();
+			}
+			});
+			break;
+			
+		case '/option1':
+		
+		
+		if(method == 'POST') {
+			processPost(request,response,function(){
+			console.log("req post: " + request.post);
+			
+			
+			var user = JSON.parse(request.post).username;
+			var password = JSON.parse(request.post).password
+			/////var res = n1+n2;
+			
+	
+			var uuu = dj.find(item => 
+				{ return item.user.username == user && item.user.password == password
+			});
+	
+			console.log(uuu);
+			
+			
+			
+			response.writeHead(200, {'Content-Type': 'application/json'});
+			//response.write(JSON.stringify(request.post));
+			response.write(JSON.stringify({result:uuu}));
+			response.end();
+		  
+		  
+	  });
+	}
+			
+			break;
+		
+	}
+	
+		
+	
+	
+	
+	
+  
+}).listen(8080); // Activates this server, listening on port 8080.
