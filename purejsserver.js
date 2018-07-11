46c551a3b5f1b728ce4bc60076ec8edce37d4713
AllTeach <33581448+AllTeach@users.noreply.github.com>
2018-07-11 11:38:25 +0300

    Update purejsserver.js

--- purejsserver.js
+++ purejsserver.js
@@ -41,6 +41,21 @@ http.createServer((request, response) => {
     // do whatever we need to in order to respond to this request.
 	console.log(url);
 	
+	// Enabling cors
+	  if (request.method === 'OPTIONS') {
+        console.log('!OPTIONS');
+        //var headers = {};
+        // IE8 does not allow domains to be specified, just the *
+        // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+        headers["Access-Control-Allow-Origin"] = "*";
+        headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+        headers["Access-Control-Allow-Credentials"] = false;
+        headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+        headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+        response.writeHead(200, headers);
+        response.end();
+    }
+	
 	response.on('error', (err) => {
 			  console.error(err);
 			});
